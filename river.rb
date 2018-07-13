a284abae7f9a92f46fa4a65e07dfabb1b1cb380f
mggbhn <maggiebswim@gmail.com>
2018-07-13 15:18:51 +0000

    Done.

--- river.rb
+++ river.rb
@@ -1,1 +1,24 @@
 # river.rb
+class River 
+  attr_accessor :name, :length, :countries, :discharge
+  
+  def initialize (name)
+    #length, countries, discharge
+    @name = name
+    # @length = length
+    # @countries = countries 
+    # @discharge = discharge
+  end
+  
+  def flood 
+   @discharge = discharge * 1.30
+end
+
+def dry_up
+  @discharge = discharge - (discharge/2)
+end
+end
+
+# mississippi = River.new("Mississippi")
+
+
